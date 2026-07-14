3360cbe2397e40f872b67b9294da6a2b8e0c53b9
MehrAz <mirajul.hossain002@gmail.com>
2026-07-14 15:14:01 +0600

    dom day 3
    Today I practiced DOM attribute manipulation using JavaScript.
    
    Topics covered:
    - getAttribute()
    - setAttribute()
    - Reading existing HTML attributes
    - Updating attributes dynamically
    - Changing image source (src)
    - Updating alt and title attributes
    
    This was a short practice session focused on understanding how JavaScript interacts with HTML element attributes.

---
+++ dom day 3/getAttribute.js
@@ -0,0 +1,7 @@
+// let heading = document.getElementById("title");
+
+// console.log(heading.getAttribute("id"));
+// console.log(heading.getAttribute("class"));
+
+// Output  = title
+//         = heading
---
+++ dom day 3/setAttribute.js
@@ -0,0 +1,10 @@
+// heading.setAttribute("class", "newHeading");
+
+// console.log(heading);
+
+// =========================================================
+let cat = document.getElementById("cat");
+let dog = document.getElementById("dog");
+
+console.log(cat.getAttribute("src"));
+console.log(dog.getAttribute("src"));
