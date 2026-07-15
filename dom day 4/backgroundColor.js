9d09bc8e4a18a62408707e9a5f04e5dc5e8f724c
MehrAz <mirajul.hossain002@gmail.com>
2026-07-15 23:56:27 +0600

    dom day 4
    🎨 JavaScript DOM - Day 4 (Part 1)
    
    Today I started DOM Day 4 by learning how to manipulate CSS styles using JavaScript.
    
    Topics Covered
    Text Color (style.color)
    Background Color (style.backgroundColor)
    What I Learned
    How to change an element's text color using JavaScript.
    How to change an element's background color dynamically.
    How the style property is used to control CSS through the DOM.
    
    🚀 I'll continue the remaining DOM Day 4 topics in the next session.

---
+++ dom day 4/backgroundColor.js
@@ -0,0 +1,27 @@
+let title = document.getElementById("title");
+
+title.style.backgroundColor = "yellow";
+
+// // ✅ Output:
+
+// // Text Background = Yellow
+
+// =========================================================
+
+let heading = document.getElementById("heading");
+
+heading.style.backgroundColor = "skyblue";
+
+// // ✅ Output:
+
+// // Text Background = skyblue
+
+// =============================================================
+
+let text = document.getElementById("text");
+
+text.style.backgroundColor = "skyblue";
+text.style.display = "block";
+text.style.width = "300px";
+text.style.backgroundColor = "skyblue";
+
