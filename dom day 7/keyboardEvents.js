f2b8cc1c27ade49a76ed9be9d1349fbe158ad723
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-07-19 22:27:49 +0600

    Practice keyboard events in DOM

---
+++ dom day 7/keyboardEvents.js
@@ -0,0 +1,94 @@
+// document.addEventListener("keydown", function(){
+//     console.log("Key Pressed");
+    
+// });
+// কী হবে?
+
+// যে কোনো key চাপো:
+
+// A
+// B
+// Enter
+// Space
+// Arrow Keys
+
+// Console:
+
+// Key Pressed
+
+// ==========================================================
+
+// document.addEventListener("keydown", function (event){
+//     console.log(event.key);
+    
+// })
+
+// Output =
+
+// যদি চাপো:
+
+// A
+
+// Console:
+
+// a
+
+// যদি চাপো:
+
+// M
+
+// Console:
+
+// m
+
+// যদি চাপো:
+
+// Enter
+
+// Console:
+
+// Enter
+
+// যদি চাপো:
+
+// Backspace
+
+// Console:
+
+// Backspace
+
+// যদি চাপো:
+
+// ArrowLeft
+
+// Console:
+
+// ArrowLeft
+
+// ======================================================
+
+// document.addEventListener("keydown", function (event){
+//     console.log(event.code);
+    
+// });
+
+// Output
+
+// Keyboard tap .....
+
+// Key                 	Output (event.code)
+// A	                        KeyA
+// B	                        KeyB
+// Enter	                    Enter
+// Space	                    Space
+// Arrow Up	                    ArrowUp
+// Shift	                   ShiftLeft বা ShiftRight
+
+// =======================================================
+
+document.addEventListener("keydown", function (event) {
+
+    console.log("Key:", event.key);
+    console.log("Code:", event.code);
+
+});
