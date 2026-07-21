0f2787128b8c288f2d26fa5d180aa892f4cff93c
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-07-21 15:16:36 +0600

    Learn classList contains and replace in DOM Day 9

---
+++ dom day 9/classListContains.js
@@ -0,0 +1,40 @@
+// classList.contains()
+// 🤔 contains() কী?
+
+// contains() হলো classList-এর একটি method।
+
+// এর কাজ হলো—
+
+// একটি element-এর মধ্যে নির্দিষ্ট class আছে কি না তা check করা।
+
+// এটা কোনো class add করে না।
+
+// এটা কোনো class remove করে না।
+
+// এটা শুধু check করে।
+
+// Element ধরছি
+const title = document.getElementById("title");
+const checkBtn = document.getElementById("checkBtn");
+
+
+// ===============================================
+// Topic : classList.contains()
+//
+// কাজ:
+// নির্দিষ্ট class আছে কি না check করে।
+//
+// Return:
+// true  -> class আছে
+// false -> class নেই
+//
+// কোনো class add/remove করে না।
+// ===============================================
+
+
+// Check Button এ click করলে
+checkBtn.addEventListener("click", function () {
+
+    console.log(title.classList.contains("red"));
+
+});
