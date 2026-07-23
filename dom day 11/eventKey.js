d229d476687fa0ead75ce27dc13e75c141ca820d
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-07-23 15:23:49 +0600

    Learn event.key in keyboard events

---
+++ dom day 11/eventKey.js
@@ -0,0 +1,27 @@
+// event.key কী?
+
+// event.key হলো যে key user চাপেছে, তার value।
+
+// অর্থাৎ, browser তোমাকে বলে:
+
+// "User কোন key চাপল?"
+
+
+
+
+
+document.addEventListener("keyup", function () {
+    console.log("Key Up");
+});
+
+// Browser-এ page open 
+// F12 → Console open 
+// কোনো একটা key চাপে ধরে rakkhi......
+// এরপর ছেড়ে di, then.........
+// 🖥️ Output
+
+// Key ছাড়ার পর Console-এ দেখাবে—
+
+// Key Up
+
+// =======================================================================
