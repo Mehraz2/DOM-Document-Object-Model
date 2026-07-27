ad0d96c3d55ed3a31c74ddc3fbf668b05ab8b78d
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-07-28 02:46:03 +0600

    Practice insertAdjacentElement afterbegin

---
+++ dom day 15/insertAdjacentElement/afterBegin.js
@@ -0,0 +1,125 @@
+
+
+// afterbegin কী করে?
+
+// afterbegin মানে:
+
+// ➡️ Target element-এর ভিতরে শুরুতে নতুন element যোগ করবে।
+
+// Structure:
+
+const container = document.getElementById("container");
+
+const paragraph = document.createElement("p");
+
+paragraph.innerText = "I am added with afterbegin";
+
+container.insertAdjacentElement("afterbegin", paragraph);
+
+
+// Output:
+
+// Before:
+
+// <div id="container">
+
+//     <h2>Existing Heading</h2>
+
+// </div>
+
+
+
+
+
+
+
+
+// After:
+
+// <div id="container">
+
+//     <p>I am added with afterbegin</p>
+
+//     <h2>Existing Heading</h2>
+
+// </div>
+// কী হলো?
+// container.insertAdjacentElement("afterbegin", paragraph);
+
+// মানে:
+
+// container → যেখানে element ঢুকবে
+// afterbegin → container-এর ভিতরে, সবার আগে
+// paragraph → যে নতুন element যোগ হচ্ছে
+
+// DOM:
+
+// Before:
+
+// div#container
+//     |
+//     └── h2
+
+
+
+
+
+
+
+
+
+
+// After:
+
+// div#container
+//     |
+//     ├── p   ← নতুন element
+//     |
+//     └── h2
+
+// ===== practice 2 ======================================================================
+
+
+const list = document.getElementById("list");
+
+const newItem = document.createElement("li");
+
+newItem.innerText = "HTML";
+
+list.insertAdjacentElement("afterbegin", newItem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
