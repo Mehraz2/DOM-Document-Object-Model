bef1a4d1cb1fe2f499f7fb0eefafe3be3b194a8f
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-08-02 15:41:55 +0600

    Complete localStorage setItem() practice 2

---
+++ dom day 17/localStorage/practice2.js
@@ -0,0 +1,21 @@
+// User input থেকে data নিয়ে localStorage-এ save করা। 
+// আগের practice-এ name save করেছিল, 
+// এবার favorite color save করব
+
+
+
+
+
+const colorInput = document.getElementById("colorInput");
+const saveBtn = document.getElementById("saveBtn");
+
+saveBtn.addEventListener("click", () => {
+
+        const favoriteColor = colorInput.value;
+
+        localStorage.setItem("favoriteColor", favoriteColor);
+
+        console.log("Favorite Color Saved");
+        
+
+})
