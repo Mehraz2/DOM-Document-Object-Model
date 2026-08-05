08b284fedfa933d38ce100e56826f5e598fea369
Mehraz Hossain <mirajul.hossain002@gmail.com>
2026-08-05 22:56:22 +0600

    Complete scroll event practice 3

---
+++ dom day 19/Scroll Events/practice3.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+// 🟢 Scroll Event — Practice 3 (Change Background Color While Scrolling)
+
+// এবার আমরা এমন একটি example করব যেটা দেখেই বোঝা যাবে scroll event কাজ করছে। 
+// Console বা Alert-এর দরকার হবে না।
+
+
+
+
+
+
+window.addEventListener("scroll3", () => {
+    document.body.style.backgroundColor = "lightblue";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
